622ff1dcada31663c840d2d461e9a5a2a2491f49
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2022-10-29 16:10:01 +0330

    Add project files.

---
+++ myfirstapp/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace myfirstapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.N1 = new System.Windows.Forms.TextBox();
+            this.V1 = new System.Windows.Forms.TextBox();
+            this.N2 = new System.Windows.Forms.TextBox();
+            this.V2 = new System.Windows.Forms.TextBox();
+            this.N3 = new System.Windows.Forms.TextBox();
+            this.V3 = new System.Windows.Forms.TextBox();
+            this.N4 = new System.Windows.Forms.TextBox();
+            this.V4 = new System.Windows.Forms.TextBox();
+            this.N5 = new System.Windows.Forms.TextBox();
+            this.V5 = new System.Windows.Forms.TextBox();
+            this.N6 = new System.Windows.Forms.TextBox();
+            this.V6 = new System.Windows.Forms.TextBox();
+            this.N7 = new System.Windows.Forms.TextBox();
+            this.V7 = new System.Windows.Forms.TextBox();
+            this.N8 = new System.Windows.Forms.TextBox();
+            this.V8 = new System.Windows.Forms.TextBox();
+            this.N9 = new System.Windows.Forms.TextBox();
+            this.V9 = new System.Windows.Forms.TextBox();
+            this.N10 = new System.Windows.Forms.TextBox();
+            this.V10 = new System.Windows.Forms.TextBox();
+            this.Nomreh = new System.Windows.Forms.Label();
+            this.Vahed = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Ans = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // N1
+            // 
+            this.N1.Location = new System.Drawing.Point(18, 40);
+            this.N1.MaxLength = 5;
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(75, 22);
+            this.N1.TabIndex = 0;
+            // 
+            // V1
+            // 
+            this.V1.Location = new System.Drawing.Point(111, 40);
+            this.V1.MaxLength = 1;
+            this.V1.Name = "V1";
+            this.V1.Size = new System.Drawing.Size(40, 22);
+            this.V1.TabIndex = 1;
+            // 
+            // N2
+            // 
+            this.N2.Location = new System.Drawing.Point(18, 77);
+            this.N2.MaxLength = 5;
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(75, 22);
+            this.N2.TabIndex = 2;
+            // 
+            // V2
+            // 
+            this.V2.Location = new System.Drawing.Point(111, 77);
+            this.V2.MaxLength = 1;
+            this.V2.Name = "V2";
+            this.V2.Size = new System.Drawing.Size(40, 22);
+            this.V2.TabIndex = 3;
+            // 
+            // N3
+            // 
+            this.N3.Location = new System.Drawing.Point(18, 117);
+            this.N3.MaxLength = 5;
+            this.N3.Name = "N3";
+            this.N3.Size = new System.Drawing.Size(75, 22);
+            this.N3.TabIndex = 4;
+            // 
+            // V3
+            // 
+            this.V3.Location = new System.Drawing.Point(111, 117);
+            this.V3.MaxLength = 1;
+            this.V3.Name = "V3";
+            this.V3.Size = new System.Drawing.Size(40, 22);
+            this.V3.TabIndex = 5;
+            // 
+            // N4
+            // 
+            this.N4.Location = new System.Drawing.Point(18, 157);
+            this.N4.MaxLength = 5;
+            this.N4.Name = "N4";
+            this.N4.Size = new System.Drawing.Size(75, 22);
+            this.N4.TabIndex = 6;
+            // 
+            // V4
+            // 
+            this.V4.Location = new System.Drawing.Point(111, 157);
+            this.V4.MaxLength = 1;
+            this.V4.Name = "V4";
+            this.V4.Size = new System.Drawing.Size(40, 22);
+            this.V4.TabIndex = 7;
+            // 
+            // N5
+            // 
+            this.N5.Location = new System.Drawing.Point(18, 197);
+            this.N5.MaxLength = 5;
+            this.N5.Name = "N5";
+            this.N5.Size = new System.Drawing.Size(75, 22);
+            this.N5.TabIndex = 8;
+            // 
+            // V5
+            // 
+            this.V5.Location = new System.Drawing.Point(111, 197);
+            this.V5.MaxLength = 1;
+            this.V5.Name = "V5";
+            this.V5.Size = new System.Drawing.Size(40, 22);
+            this.V5.TabIndex = 9;
+            // 
+            // N6
+            // 
+            this.N6.Location = new System.Drawing.Point(18, 236);
+            this.N6.MaxLength = 5;
+            this.N6.Name = "N6";
+            this.N6.Size = new System.Drawing.Size(75, 22);
+            this.N6.TabIndex = 10;
+            // 
+            // V6
+            // 
+            this.V6.Location = new System.Drawing.Point(111, 236);
+            this.V6.MaxLength = 1;
+            this.V6.Name = "V6";
+            this.V6.Size = new System.Drawing.Size(40, 22);
+            this.V6.TabIndex = 11;
+            // 
+            // N7
+            // 
+            this.N7.Location = new System.Drawing.Point(18, 275);
+            this.N7.MaxLength = 5;
+            this.N7.Name = "N7";
+            this.N7.Size = new System.Drawing.Size(75, 22);
+            this.N7.TabIndex = 12;
+            // 
+            // V7
+            // 
+            this.V7.Location = new System.Drawing.Point(111, 275);
+            this.V7.MaxLength = 1;
+            this.V7.Name = "V7";
+            this.V7.Size = new System.Drawing.Size(40, 22);
+            this.V7.TabIndex = 13;
+            // 
+            // N8
+            // 
+            this.N8.Location = new System.Drawing.Point(18, 318);
+            this.N8.MaxLength = 5;
+            this.N8.Name = "N8";
+            this.N8.Size = new System.Drawing.Size(75, 22);
+            this.N8.TabIndex = 14;
+            // 
+            // V8
+            // 
+            this.V8.Location = new System.Drawing.Point(111, 318);
+            this.V8.MaxLength = 1;
+            this.V8.Name = "V8";
+            this.V8.Size = new System.Drawing.Size(40, 22);
+            this.V8.TabIndex = 15;
+            // 
+            // N9
+            // 
+            this.N9.Location = new System.Drawing.Point(18, 358);
+            this.N9.MaxLength = 5;
+            this.N9.Name = "N9";
+            this.N9.Size = new System.Drawing.Size(75, 22);
+            this.N9.TabIndex = 16;
+            // 
+            // V9
+            // 
+            this.V9.Location = new System.Drawing.Point(111, 358);
+            this.V9.MaxLength = 1;
+            this.V9.Name = "V9";
+            this.V9.Size = new System.Drawing.Size(40, 22);
+            this.V9.TabIndex = 17;
+            // 
+            // N10
+            // 
+            this.N10.Location = new System.Drawing.Point(18, 399);
+            this.N10.MaxLength = 5;
+            this.N10.Name = "N10";
+            this.N10.Size = new System.Drawing.Size(75, 22);
+            this.N10.TabIndex = 18;
+            // 
+            // V10
+            // 
+            this.V10.Location = new System.Drawing.Point(111, 399);
+            this.V10.MaxLength = 1;
+            this.V10.Name = "V10";
+            this.V10.Size = new System.Drawing.Size(40, 22);
+            this.V10.TabIndex = 19;
+            // 
+            // Nomreh
+            // 
+            this.Nomreh.AutoSize = true;
+            this.Nomreh.Font = new System.Drawing.Font("B Nazanin", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Nomreh.Location = new System.Drawing.Point(37, 8);
+            this.Nomreh.Name = "Nomreh";
+            this.Nomreh.Size = new System.Drawing.Size(39, 29);
+            this.Nomreh.TabIndex = 20;
+            this.Nomreh.Text = "نمره";
+            // 
+            // Vahed
+            // 
+            this.Vahed.AutoSize = true;
+            this.Vahed.Font = new System.Drawing.Font("B Nazanin", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Vahed.Location = new System.Drawing.Point(108, 8);
+            this.Vahed.Name = "Vahed";
+            this.Vahed.Size = new System.Drawing.Size(43, 29);
+            this.Vahed.TabIndex = 21;
+            this.Vahed.Text = "واحد";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(96, 482);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "محاسبه";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ans
+            // 
+            this.Ans.AutoSize = true;
+            this.Ans.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans.Location = new System.Drawing.Point(4, 3);
+            this.Ans.Name = "Ans";
+            this.Ans.Size = new System.Drawing.Size(0, 19);
+            this.Ans.TabIndex = 23;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.Ans);
+            this.panel1.Location = new System.Drawing.Point(11, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 49);
+            this.panel1.TabIndex = 24;
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.OrangeRed;
+            this.Clear.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Clear.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Clear.Location = new System.Drawing.Point(7, 482);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(79, 37);
+            this.Clear.TabIndex = 25;
+            this.Clear.Text = "پاکسازی ";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.OrangeRed;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Font = new System.Drawing.Font("B Nazanin", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Exit.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Exit.Location = new System.Drawing.Point(176, 3);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(27, 26);
+            this.Exit.TabIndex = 26;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(207, 529);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Vahed);
+            this.Controls.Add(this.Nomreh);
+            this.Controls.Add(this.V10);
+            this.Controls.Add(this.N10);
+            this.Controls.Add(this.V9);
+            this.Controls.Add(this.N9);
+            this.Controls.Add(this.V8);
+            this.Controls.Add(this.N8);
+            this.Controls.Add(this.V7);
+            this.Controls.Add(this.N7);
+            this.Controls.Add(this.V6);
+            this.Controls.Add(this.N6);
+            this.Controls.Add(this.V5);
+            this.Controls.Add(this.N5);
+            this.Controls.Add(this.V4);
+            this.Controls.Add(this.N4);
+            this.Controls.Add(this.V3);
+            this.Controls.Add(this.N3);
+            this.Controls.Add(this.V2);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.V1);
+            this.Controls.Add(this.N1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "محاسبه معدل";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox N1;
+        private System.Windows.Forms.TextBox V1;
+        private System.Windows.Forms.TextBox N2;
+        private System.Windows.Forms.TextBox V2;
+        private System.Windows.Forms.TextBox N3;
+        private System.Windows.Forms.TextBox V3;
+        private System.Windows.Forms.TextBox N4;
+        private System.Windows.Forms.TextBox V4;
+        private System.Windows.Forms.TextBox N5;
+        private System.Windows.Forms.TextBox V5;
+        private System.Windows.Forms.TextBox N6;
+        private System.Windows.Forms.TextBox V6;
+        private System.Windows.Forms.TextBox N7;
+        private System.Windows.Forms.TextBox V7;
+        private System.Windows.Forms.TextBox N8;
+        private System.Windows.Forms.TextBox V8;
+        private System.Windows.Forms.TextBox N9;
+        private System.Windows.Forms.TextBox V9;
+        private System.Windows.Forms.TextBox N10;
+        private System.Windows.Forms.TextBox V10;
+        private System.Windows.Forms.Label Nomreh;
+        private System.Windows.Forms.Label Vahed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Ans;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ myfirstapp/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace myfirstapp
+{
+
+    public partial class Form1 : Form
+    {
+        public double[] Scores = new double[10];
+
+        public int[] Vaheds = new int[10];
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (V1.Text != "")
+            {
+                Vaheds[0] = Convert.ToInt16(V1.Text);
+            }
+            else
+            {
+                Vaheds[0] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V2.Text != "")
+            {
+                Vaheds[1] = Convert.ToInt16(V2.Text);
+            }
+            else
+            {
+                Vaheds[1] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V3.Text != "")
+            {
+                Vaheds[2] = Convert.ToInt16(V3.Text);
+            }
+            else
+            {
+                Vaheds[2] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V4.Text != "")
+            {
+                Vaheds[3] = Convert.ToInt16(V4.Text);
+            }
+            else
+            {
+                Vaheds[3] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V5.Text != "")
+            {
+                Vaheds[4] = Convert.ToInt16(V5.Text);
+            }
+            else
+            {
+                Vaheds[4] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V6.Text != "")
+            {
+                Vaheds[5] = Convert.ToInt16(V6.Text);
+            }
+            else
+            {
+                Vaheds[5] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V7.Text != "")
+            {
+                Vaheds[6] = Convert.ToInt16(V7.Text);
+            }
+            else
+            {
+                Vaheds[6] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V8.Text != "")
+            {
+                Vaheds[7] = Convert.ToInt16(V8.Text);
+            }
+            else
+            {
+                Vaheds[7] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V9.Text != "")
+            {
+                Vaheds[8] = Convert.ToInt16(V9.Text);
+            }
+            else
+            {
+                Vaheds[8] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (V10.Text != "")
+            {
+                Vaheds[9] = Convert.ToInt16(V10.Text);
+            }
+            else
+            {
+                Vaheds[9] = 0;
+            }
+
+
+
+
+            //-----------------------------------------------------------------------------------------------
+
+
+
+            if (N1.Text != "")
+            {
+                Scores[0] = Convert.ToDouble(N1.Text);
+            }
+            else
+            {
+                Scores[0] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N2.Text != "")
+            {
+                Scores[1] = Convert.ToDouble(N2.Text);
+            }
+            else
+            {
+                Scores[1] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N3.Text != "")
+            {
+                Scores[2] = Convert.ToDouble(N3.Text);
+            }
+            else
+            {
+                Scores[2] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N4.Text != "")
+            {
+                Scores[3] = Convert.ToDouble(N4.Text);
+            }
+            else
+
+            {
+                Scores[3] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N5.Text != "")
+            {
+                Scores[4] = Convert.ToDouble(N5.Text);
+            }
+            else
+            {
+                Scores[4] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N6.Text != "")
+            {
+                Scores[5] = Convert.ToDouble(N6.Text);
+            }
+            else
+            {
+                Scores[5] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N7.Text != "")
+            {
+                Scores[6] = Convert.ToDouble(N7.Text);
+            }
+            else
+            {
+                Scores[6] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N8.Text != "")
+            {
+                Scores[7] = Convert.ToDouble(N8.Text);
+            }
+            else
+            {
+                Scores[7] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N9.Text != "")
+            {
+                Scores[8] = Convert.ToDouble(N9.Text);
+            }
+            else
+            {
+                Scores[8] = 0;
+            }
+
+            //--------------------------------------------------
+
+            if (N10.Text != "")
+            {
+                Scores[9] = Convert.ToDouble(N10.Text);
+            }
+            else
+            {
+                Scores[9] = 0;
+            }
+
+            Double SumN = 0;
+            int SumV = 0;
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                SumN += Scores[i];
+                SumV += Vaheds[i];
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(SumV);
+                Console.WriteLine(SumN);
+            }
+
+            Double Answer = SumN / SumV;
+
+            Ans.Text = Answer.ToString();
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            N1.Text = "";
+            N2.Text = "";
+            N3.Text = "";
+            N4.Text = "";
+            N5.Text = "";
+            N6.Text = "";
+            N7.Text = "";
+            N8.Text = "";
+            N9.Text = "";
+            N10.Text = "";
+
+            V1.Text = "";
+            V2.Text = "";
+            V3.Text = "";
+            V4.Text = "";
+            V5.Text = "";
+            V6.Text = "";
+            V7.Text = "";
+            V8.Text = "";
+            V9.Text = "";
+            V10.Text = "";
+        }
+    }
+}
